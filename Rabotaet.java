938ef68a236072faec71118f1566412313f61cde
Alex42rr <kotlyarovavsz@gmail.com>
2025-04-09 16:16:48 +0700

    Высылаю ответку в хаб.

---
+++ Rabotaet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Rabotaet {
+    public static void main(String[] args)
+    {
+        String str = "10 20 40 60 100";
+        Scanner scanner = new Scanner(str);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int e = scanner.nextInt();
+
+        System.out.println(a + b - c - d + e);
+    }
+}
+
